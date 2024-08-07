98b0c6583eda71952731166aa944961fdd8000b0
gilpop8663 <wolfye@naver.com>
2024-08-07 21:55:24 +0900

    test: 미블, 리뷰노트 링크를 입력하여 캠페인 생성 테스트

--- test/02-campaigns.e2e-spec.ts
+++ test/02-campaigns.e2e-spec.ts
@@ -4,15 +4,24 @@ import 'expect-puppeteer';
 
 const GRAPHQL_ENDPOINT = '/graphql';
 
-test('강남맛집 링크를 입력해서 방문형 캠페인을 생성한다.', async () => {
-  const [initialUser] = await usersRepository.find();
+test.each([
+  ['강남맛집', '방문형', 'https://xn--939au0g4vj8sq.net/cp/?id=1461979'],
+  ['강남맛집', '배송형', 'https://xn--939au0g4vj8sq.net/cp/?id=1474045'],
+  ['레뷰', '방문형', 'https://www.revu.net/campaign/987487'],
+  ['레뷰', '배송형', 'https://www.revu.net/campaign/988004'],
+  ['리뷰노트', '방문형', 'https://www.reviewnote.co.kr/campaigns/342622'],
+  ['리뷰노트', '배송형', 'https://www.reviewnote.co.kr/campaigns/340748'],
+  ['미블', '방문형', 'https://www.mrblog.net/campaigns/755759'],
+  ['미블', '배송형', 'https://www.mrblog.net/campaigns/752638'],
+])(
+  '%s 링크를 입력해서 %s 캠페인을 생성한다.',
+  async (platformName, deliveryType, linkUrl) => {
+    const [initialUser] = await usersRepository.find();
 
-  const linkUrl = 'https://xn--939au0g4vj8sq.net/cp/?id=1461979';
-
-  await request(app.getHttpServer())
-    .post(GRAPHQL_ENDPOINT)
-    .send({
-      query: /* GraphQL */ `
+    await request(app.getHttpServer())
+      .post(GRAPHQL_ENDPOINT)
+      .send({
+        query: /* GraphQL */ `
         mutation {
           createCampaignFromLink(
             input: {
@@ -26,152 +35,24 @@ test('강남맛집 링크를 입력해서 방문형 캠페인을 생성한다.',
           }
         }
       `,
-    })
-    .expect(200)
-    .expect((res) => {
-      const {
-        body: {
-          data: { createCampaignFromLink },
-        },
-      } = res;
+      })
+      .expect(200)
+      .expect((res) => {
+        const {
+          body: {
+            data: { createCampaignFromLink },
+          },
+        } = res;
 
-      expect(createCampaignFromLink.ok).toBe(true);
-      expect(createCampaignFromLink.error).toBe(null);
-      expect(createCampaignFromLink.campaignId).toEqual(expect.any(Number));
+        expect(createCampaignFromLink.ok).toBe(true);
+        expect(createCampaignFromLink.error).toBe(null);
+        expect(createCampaignFromLink.campaignId).toEqual(expect.any(Number));
+      });
+
+    const initialCampaign = await campaignRepository.findOne({
+      where: { detailedViewLink: linkUrl },
     });
 
-  const initialCampaign = await campaignRepository.findOne({
-    where: { detailedViewLink: linkUrl },
-  });
-
-  expect(initialCampaign).toBeDefined();
-});
-
-test('강남맛집 링크를 입력해서 배송형 캠페인을 생성한다.', async () => {
-  const [initialUser] = await usersRepository.find();
-
-  const linkUrl = 'https://xn--939au0g4vj8sq.net/cp/?id=1474045';
-
-  await request(app.getHttpServer())
-    .post(GRAPHQL_ENDPOINT)
-    .send({
-      query: /* GraphQL */ `
-        mutation {
-          createCampaignFromLink(
-            input: {
-              linkUrl: "${linkUrl}"
-              userId: ${initialUser.id}
-            }
-          ) {
-            ok
-            error
-            campaignId
-          }
-        }
-      `,
-    })
-    .expect(200)
-    .expect((res) => {
-      const {
-        body: {
-          data: { createCampaignFromLink },
-        },
-      } = res;
-
-      expect(createCampaignFromLink.ok).toBe(true);
-      expect(createCampaignFromLink.error).toBe(null);
-      expect(createCampaignFromLink.campaignId).toEqual(expect.any(Number));
-    });
-
-  const initialCampaign = await campaignRepository.findOne({
-    where: { detailedViewLink: linkUrl },
-  });
-
-  expect(initialCampaign).toBeDefined();
-});
-
-test('방문형 레뷰 링크를 입력해서 캠페인을 생성한다.', async () => {
-  const [initialUser] = await usersRepository.find();
-
-  const linkUrl = 'https://www.revu.net/campaign/987487';
-
-  await request(app.getHttpServer())
-    .post(GRAPHQL_ENDPOINT)
-    .send({
-      query: /* GraphQL */ `
-        mutation {
-          createCampaignFromLink(
-            input: {
-              linkUrl: "${linkUrl}"
-              userId: ${initialUser.id}
-            }
-          ) {
-            ok
-            error
-            campaignId
-          }
-        }
-      `,
-    })
-    .expect(200)
-    .expect((res) => {
-      const {
-        body: {
-          data: { createCampaignFromLink },
-        },
-      } = res;
-
-      expect(createCampaignFromLink.ok).toBe(true);
-      expect(createCampaignFromLink.error).toBe(null);
-      expect(createCampaignFromLink.campaignId).toEqual(expect.any(Number));
-    });
-
-  const initialCampaign = await campaignRepository.findOne({
-    where: { detailedViewLink: linkUrl },
-  });
-
-  expect(initialCampaign).toBeDefined();
-});
-
-test('배송형 레뷰 링크를 입력해서 캠페인을 생성한다.', async () => {
-  const [initialUser] = await usersRepository.find();
-
-  const linkUrl = 'https://www.revu.net/campaign/988004';
-
-  await request(app.getHttpServer())
-    .post(GRAPHQL_ENDPOINT)
-    .send({
-      query: /* GraphQL */ `
-        mutation {
-          createCampaignFromLink(
-            input: {
-              linkUrl: "${linkUrl}"
-              userId: ${initialUser.id}
-            }
-          ) {
-            ok
-            error
-            campaignId
-          }
-        }
-      `,
-    })
-    .expect(200)
-    .expect((res) => {
-      const {
-        body: {
-          data: { createCampaignFromLink },
-        },
-      } = res;
-
-      expect(createCampaignFromLink.ok).toBe(true);
-      expect(createCampaignFromLink.error).toBe(null);
-      expect(createCampaignFromLink.campaignId).toEqual(expect.any(Number));
-    });
-
-  const initialCampaign = await campaignRepository.findOne({
-    where: { detailedViewLink: linkUrl },
-  });
-
-  expect(initialCampaign).toBeDefined();
-});
+    expect(initialCampaign).toBeDefined();
+  },
+);

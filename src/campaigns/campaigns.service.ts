8abfd3b0fd1328e09f3a1bdb1d5cb38bcee8312d
gilpop8663 <wolfye@naver.com>
2024-08-07 21:47:03 +0900

    feat: 레뷰 링크를 입력하여 캠페인 생성 기능 구현

--- src/campaigns/campaigns.service.ts
+++ src/campaigns/campaigns.service.ts
@@ -10,6 +10,7 @@ import {
 } from './dtos/create-campaign-link.dto';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import puppeteer from 'puppeteer';
 
 @Injectable()
 export class CampaignsService {
@@ -25,51 +26,29 @@ export class CampaignsService {
     userId,
   }: CreateCampaignLinkInput): Promise<CreateCampaignLinkOutput> {
     try {
+      const platformName = this.getPlatformName(linkUrl);
+
       const user = await this.userRepository.findOne({ where: { id: userId } });
 
       if (!user) {
         return { ok: false, error: '유저가 존재하지 않습니다.' };
       }
 
-      const response = await axios.get(linkUrl, {
-        headers: {
-          setUserAgent:
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
-        },
-      });
+      let campaign: Campaign;
 
-      const html = response.data;
+      if (platformName === '강남맛집') {
+        campaign = await this.getGangnamMatzipCampaign(user, linkUrl);
+      } else if (platformName === '레뷰') {
+        campaign = await this.getRevuCampaign(user, linkUrl);
+      }
 
-      const $ = cheerio.load(html);
-
-      const title = $('.tit', '.textArea').text().trim();
-      const thumbnailUrl = $('#img').attr('src');
-      const experienceType =
-        $('.type').text() === '방문형'
-          ? ExperienceType.visitType
-          : ExperienceType.deliveryType;
-      const reviewDeadline = this.getDeadlineDate($('dd', '.on').text());
-      const serviceDetails = $('.sub_tit', '.textArea').text().trim();
-      const location = $('#cont_map').next().text().trim();
-
-      const platformName = this.getPlatformName(linkUrl);
-
-      const campaign = this.campaignRepository.create({
-        user,
-        title,
-        detailedViewLink: linkUrl,
-        platformName,
-        thumbnailUrl,
-        experienceType,
-        reviewDeadline,
-        serviceDetails,
-        location,
-      });
+      console.log(campaign);
 
       await this.campaignRepository.save(campaign);
 
       return { ok: true, campaignId: campaign.id };
     } catch (error) {
+      console.error(error);
       return logErrorAndReturnFalse(error, '캠페인 생성에 실패했습니다.');
     }
   }
@@ -79,11 +58,98 @@ export class CampaignsService {
 
     const SITE_NAME = {
       강남맛집: 'xn--939au0g4vj8sq',
+      레뷰: 'www.revu.net',
     };
 
     if (parsedLinkUrl.includes(SITE_NAME.강남맛집)) {
       return '강남맛집';
     }
+
+    if (parsedLinkUrl.includes(SITE_NAME.레뷰)) {
+      return '레뷰';
+    }
+  }
+
+  async getGangnamMatzipCampaign(user: User, linkUrl: string) {
+    const response = await axios.get(linkUrl, {
+      headers: {
+        setUserAgent:
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+      },
+    });
+
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    const title = $('.tit', '.textArea').text().trim();
+    const thumbnailUrl = $('#img').attr('src');
+    const experienceType =
+      $('.type').text() === '방문형'
+        ? ExperienceType.visitType
+        : ExperienceType.deliveryType;
+    const reviewDeadline = this.getDeadlineDate($('dd', '.on').text());
+    const serviceDetails = $('.sub_tit', '.textArea').text().trim();
+    const location = $('#cont_map').next().text().trim();
+
+    const campaign = this.campaignRepository.create({
+      user,
+      title,
+      detailedViewLink: linkUrl,
+      platformName: '강남맛집',
+      thumbnailUrl,
+      experienceType,
+      reviewDeadline,
+      serviceDetails,
+      location,
+    });
+
+    return campaign;
+  }
+
+  async getRevuCampaign(user: User, linkUrl: string) {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1366,
+      height: 768,
+    });
+
+    await page.goto(linkUrl);
+
+    const content = await page.content();
+
+    const $ = cheerio.load(content);
+
+    await browser.close();
+
+    const title = $('h2').text().trim();
+    const thumbnailUrl = $('.campaign-title-img').attr('src');
+    const experienceType =
+      $('.vat').text() === '방문 및 예약안내'
+        ? ExperienceType.visitType
+        : ExperienceType.deliveryType;
+    const reviewDeadline = this.getDeadlineDate(
+      $('.con.ng-binding').eq(2).text(),
+    );
+    const serviceDetails = $('.ng-binding', '#scrollspy-reward').text().trim();
+    const location = $('.desc.address.ng-binding').text().trim();
+
+    const campaign = this.campaignRepository.create({
+      user,
+      title,
+      detailedViewLink: linkUrl,
+      platformName: '레뷰',
+      thumbnailUrl,
+      experienceType,
+      reviewDeadline,
+      serviceDetails,
+      location,
+    });
+
+    return campaign;
   }
 
   getDeadlineDate(dateString: string) {
@@ -96,14 +162,16 @@ export class CampaignsService {
     const today = new Date(); // 오늘 날짜
 
     // Date 객체 생성
-    const endDate = new Date(currentYear, month - 1, day); // 월은 0부터 시작하므로 -1
+    const endDate = new Date(currentYear, month - 1, day, 23, 59, 59); // 월은 0부터 시작하므로 -1
 
     // 만약 endDate가 오늘 날짜보다 이전이라면, 다음 연도로 설정
     if (endDate < today) {
       endDate.setFullYear(currentYear + 1);
     }
 
-    return endDate;
+    const koreanEndDate = new Date(endDate.getTime() + 9 * 60 * 60 * 1000); // 9시간 추가
+
+    return koreanEndDate;
   }
 
   // async deleteSaga({ sagaId }: DeleteSagaInput) {
